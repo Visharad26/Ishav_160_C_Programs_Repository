c55707f767fd1204e513464e1d727d37d914bc8b
ishavverma <2020a1r160@mietjammu.in>
2021-03-20 00:45:56 +0530

    Data type variations.

--- 001_Hello.c
+++ 001_Hello.c
@@ -1,8 +1,8 @@
 //A basic C Program to print Hello World.				© Ishav Verma 18/March/2021
 //Start
 #include<stdio.h> //Including standard input ouput header files from library.
-void main(){ 
+char main(){ 
 	printf("Hello Buddy!!");//Here we entered the texted to be printed.
-	//return 3.000;//Returning.
+	return 3;//Returning.
 } 
 //End
