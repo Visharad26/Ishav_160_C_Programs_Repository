40c5edd97c415e43da0fceb63e2f889928a038b2
ishavverma <2020a1r160@mietjammu.in>
2021-03-19 14:17:43 +0530

    A C Program to search an element in the array based on user input.

--- 018_SearchArr.c
+++ 018_SearchArr.c
@@ -1,19 +1,19 @@
-// A C Program to input N numbers and store them in an array.        © Ishav Verma 19/March/2021
+// A C Program to search an element in the array based on user choice.        © Ishav Verma 19/March/2021
 #include <stdio.h>
 void main(){  //In this program we are not using 'return 0', So 'int main() -> void main()'.
     int num;
     int i,  keynum, found = 0;
  
-    printf("Enter the number of elements ");
+    printf("Enter the number of elements: \n");
     scanf("%d", &num);//This will set the maximum number (Limit) of values to be entered by user based on their choice.
     int array[num];
-    printf("Enter the elements one by one \n");
+    printf("Enter the elements one by one: \n");
     for (i = 0; i < num; i++)
     {
         scanf("%d", &array[i]); //This will scan the user input.
     }
  
-    printf("Enter the element to be searched ");
+    printf("Enter the element to be searched: ");
     scanf("%d", &keynum);
     // This step will determine Linear search.
     for (i = 0; i < num ; i++)
@@ -26,7 +26,7 @@ void main(){  //In this program we are not using 'return 0', So 'int main() -> v
     }
     //This will give the output based on user input.
     if (found == 1) 
-        printf("Element is present in the array at position %d",i+1);
+        printf("Element is present in the array at position %d .",i+1);
     else
-        printf("Element is not present in the array\n");
+        printf("Element is not present in the array.\n");
 }
