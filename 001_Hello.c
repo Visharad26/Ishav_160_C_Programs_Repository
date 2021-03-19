c0504bc15a54ac6be8a41f2d4626920387819ee6
ishavverma <2020a1r160@mietjammu.in>
2021-03-20 00:38:18 +0530

    Testing data types

--- 001_Hello.c
+++ 001_Hello.c
@@ -1,8 +1,8 @@
 //A basic C Program to print Hello World.				© Ishav Verma 18/March/2021
 //Start
 #include<stdio.h> //Including standard input ouput header files from library.
-int main(){ 
+void main(){ 
 	printf("Hello Buddy!!");//Here we entered the texted to be printed.
-	return 0;//Returning.
+	//return 3.000;//Returning.
 } 
 //End
